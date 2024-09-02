51ca0f5813d5842593a084dce0327c82d9e1f56a
lep13 <lepakshi1304@gmail.com>
2024-09-02 16:18:33 +0530

    s3 bucket deploy changes

--- scripts/create-eb-environment.sh
+++ scripts/create-eb-environment.sh
@@ -6,13 +6,20 @@ ENV_NAME=${EB_ENV_NAME}
 S3_BUCKET=${S3_BUCKET_NAME}
 REGION=${AWS_REGION}
 
-# Create S3 bucket if not exists
+# Create S3 bucket if not exists (Reverting to the version that worked)
 if aws s3api head-bucket --bucket "$S3_BUCKET" 2>/dev/null; then
     echo "S3 bucket $S3_BUCKET already exists."
 else
     echo "Creating S3 bucket $S3_BUCKET..."
     aws s3 mb s3://$S3_BUCKET --region $REGION
 fi
+
+# Package application
+zip -r application.zip .
+
+# Upload package to S3
+echo "Uploading application.zip to S3..."
+aws s3 cp application.zip s3://$S3_BUCKET/application.zip
 
 # Create Elastic Beanstalk Application if not exists
 if aws elasticbeanstalk describe-applications --application-names $APP_NAME | grep -q $APP_NAME; then
@@ -22,26 +29,28 @@ else
     aws elasticbeanstalk create-application --application-name $APP_NAME --region $REGION
 fi
 
-# Package application
-zip -r application.zip .
-
-# Upload package to S3
-aws s3 cp application.zip s3://$S3_BUCKET/application.zip
-
 # Create new application version
-aws elasticbeanstalk create-application-version --application-name $APP_NAME --version-label v1 --source-bundle S3Bucket="$S3_BUCKET",S3Key="application.zip" --region $REGION
+echo "Creating new application version..."
+aws elasticbeanstalk create-application-version \
+    --application-name $APP_NAME \
+    --version-label "v1" \
+    --source-bundle S3Bucket="$S3_BUCKET",S3Key="application.zip" \
+    --region $REGION
 
-# Create Elastic Beanstalk Environment if not exists
-if aws elasticbeanstalk describe-environments --application-name $APP_NAME --environment-names $ENV_NAME | grep -q $ENV_NAME; then
-    echo "Elastic Beanstalk environment $ENV_NAME already exists."
+# Check if the environment exists and create or update accordingly
+existing_env=$(aws elasticbeanstalk describe-environments --application-name $APP_NAME --environment-names $ENV_NAME --query 'Environments[0].EnvironmentName' --output text)
+
+if [ "$existing_env" == "$ENV_NAME" ]; then
+    echo "Updating existing Elastic Beanstalk environment $ENV_NAME..."
+    aws elasticbeanstalk update-environment --environment-name $ENV_NAME --version-label "v1" --region $REGION
 else
-    echo "Creating Elastic Beanstalk environment $ENV_NAME..."
+    echo "Creating new Elastic Beanstalk environment $ENV_NAME..."
     aws elasticbeanstalk create-environment \
         --application-name $APP_NAME \
         --environment-name $ENV_NAME \
-        --version-label v1 \
-        --solution-stack-name "64bit Amazon Linux 2 v3.3.6 running Go 1.x" \
-        --option-settings Namespace=aws:elasticbeanstalk:environment,OptionName=EnvironmentType,Value=LoadBalanced \
+        --solution-stack-name "64bit Amazon Linux 2 v3.3.8 running Go 1.x" \
+        --version-label "v1" \
+        --option-settings Namespace=aws:elasticbeanstalk:environment,OptionName=EnvironmentType,Value=SingleInstance \
         --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=MONGO_URI,Value=${MONGO_URI} \
         --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=DB_NAME,Value=${DB_NAME} \
         --region $REGION
